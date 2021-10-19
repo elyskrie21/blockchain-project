c61e397a52b83dc2d784971419d684b124aca8e3
Elyse Kriegel <esamkriegel@gmail.com>
2021-10-19 00:12:42 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -18,18 +18,27 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
 import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 // pages
+import ProfilePage from "views/ProfilePage.js";
 import Index from "views/index"; 
 // others
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Index></Index>
-  </React.StrictMode>, 
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Redirect to="/index" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
